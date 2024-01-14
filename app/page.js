7bd4cd19f6c69daa22b169f7553a158afafd80f0
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-01-15 01:56:30 +0600

    Starting

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
-import Image from "next/image";
 import React from "react";
-import twitter from "../public/images/twitter.png";
 
-export default function page() {
+const page = () => {
   return (
-    <>
-      <Image alt="image" src={twitter} width={300} height={300} />
-    </>
+    <div className="w-full h-full flex justify-center items-center relative bg-black ">
+      <div className="max-w-screen-lg w-full h-full relative">
+        <section className="fixed w-72 flex flex-col "> Left sideBar </section>
+        <main> Home Timeline </main>
+        <section> Right Section </section>
+      </div>
+    </div>
   );
-}
+};
+
+export default page;
